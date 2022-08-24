3f8790668ca8818fa2b3707d44a905ac5fb52f50
savarapu manohar joshi <83489094+Manohar-mj@users.noreply.github.com>
2022-08-25 03:24:30 +0530

    Create Start Coding_Java.java
    When learning a new language, we first learn to output some message. Here, we'll start with the famous Hello World message. Now, here you are given a function to complete. Don't worry about the ins and outs of functions, just add the command (System.out.print("Hello World")) to print Hello World.
    
    Example:
    
    Input:
    No input
    
    Output:
    Hello World
    
    Explanation:
    Hello World is printed.

---
+++ Start Coding_Java.java
@@ -0,0 +1,28 @@
+//Start Coding - Java
+
+class Geeks{
+    
+    // Function to print hello
+    static void printHello(){
+        
+        // Your code here
+        System.out.print("Hello World");
+        
+    }
+    
+}
+
+//{ Driver Code Starts.
+
+class GfG{
+    
+    public static void main(String args[]){
+       
+       //Creating an Object of Class Geeks
+        Geeks g = new Geeks();
+        
+        //Calling printHello() function of the Class Geeks
+        g.printHello();
+    }
+    
+}
