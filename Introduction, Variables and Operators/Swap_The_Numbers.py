be6c12652c09b8ba415549f892929044c37d9615
savarapu manohar joshi <83489094+Manohar-mj@users.noreply.github.com>
2022-08-26 08:41:50 +0530

    Create Swap_The_Numbers.py

---
+++ Introduction, Variables and Operators/Swap_The_Numbers.py
@@ -0,0 +1,11 @@
+class Solution
+{
+    public void utility(int a, int b){
+        //code here
+        int temp=a;
+        a=b;
+        b=temp;
+        
+        System.out.println(a+" "+b);
+    }
+}
