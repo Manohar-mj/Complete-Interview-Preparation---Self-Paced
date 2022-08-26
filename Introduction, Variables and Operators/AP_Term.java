007ed517f2e163277d419031f686c6a5c3e8cb89
savarapu manohar joshi <83489094+Manohar-mj@users.noreply.github.com>
2022-08-26 08:46:08 +0530

    Create AP_Term.java

---
+++ Introduction, Variables and Operators/AP_Term.java
@@ -0,0 +1,8 @@
+public static void utility(int a, int d, int n){
+    //Complete the code above
+    int ans = a+(n-1)*d;
+    //Complete the code above
+    
+    //The line below prints the output. Don't change it!
+    System.out.println(ans);
+}
