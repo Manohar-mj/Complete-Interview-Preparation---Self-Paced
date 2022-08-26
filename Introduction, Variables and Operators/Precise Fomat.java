eeebfcd7edd0c776b37d966416e628646664c6aa
savarapu manohar joshi <83489094+Manohar-mj@users.noreply.github.com>
2022-08-26 08:40:57 +0530

    Create Precise Fomat.java

---
+++ Introduction, Variables and Operators/Precise Fomat.java
@@ -0,0 +1,12 @@
+class Geeks{
+    
+    static void printInFormat(float a, float b){
+        float result = a/b;
+        
+        
+        // Your code here to print upto 3 decimal placesse
+        System.out.format(result+" "+"%.3f",result);
+        
+    }    
+    
+}
