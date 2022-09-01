2c604cf489f7d786a87df8d6654fb0151e1fbc07
savarapu manohar joshi <83489094+Manohar-mj@users.noreply.github.com>
2022-09-01 23:38:21 +0530

    Create arraySum.java

---
+++ Arrays & String/arraySum.java
@@ -0,0 +1,9 @@
+public static int arraySum(int[] numbers){
+    //Just return the sum of the list
+    //Don't print herem
+    int sum=0;
+    for(int i=0;i<numbers.length;i++){
+        sum+=numbers[i];
+    }
+    return sum;
+}
