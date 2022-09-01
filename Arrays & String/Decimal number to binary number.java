43c82df8692b5430a0fc362016f37cf82558aeb4
savarapu manohar joshi <83489094+Manohar-mj@users.noreply.github.com>
2022-09-01 23:44:45 +0530

    Create Decimal number to binary number.java

---
+++ Arrays & String/Decimal number to binary number.java
@@ -0,0 +1,9 @@
+class Solution
+{
+    public void toBinary(String N)
+    {
+        // Code herern
+         int n = Integer.parseInt(N);
+       System.out.print(Integer.toBinaryString(n));
+    }
+}
