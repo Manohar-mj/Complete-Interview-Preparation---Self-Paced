f7c86eabd50947303aad3e0c6d7371179cd94c37
savarapu manohar joshi <83489094+Manohar-mj@users.noreply.github.com>
2022-09-01 23:37:35 +0530

    Create Largest Element in Array.java

---
+++ Arrays & String/Largest Element in Array.java
@@ -0,0 +1,10 @@
+    public int largest(int arr[], int n)
+    {
+        int max= arr[0];
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        return max;
+    }
