1222de0d10ebe3ccceae64f6a2c275f6d1f0ef0b
savarapu manohar joshi <83489094+Manohar-mj@users.noreply.github.com>
2022-09-01 23:43:13 +0530

    Create Reverse String.java

---
+++ Arrays & String/Reverse String.java
@@ -0,0 +1,8 @@
+// the function should return the reversed string
+public static String reverseString(String s){
+    // code hereuiv
+    
+    StringBuilder sb=new StringBuilder(s);
+    sb.reverse();
+    return sb.toString();
+}
